bfe70ab8e415499fc15375a141b761b0b541abc0
ParryQiu <qiupengyuan@gmail.com>
2017-06-05 15:23:12 +0800

    #015 - Webpack 2 中的文件压缩。

---
+++ 015/app/index.js
@@ -0,0 +1,4 @@
+import component from './component';
+import style1 from './style1.css';
+import style2 from './style2.css';
+document.body.appendChild(component('123456',style1.class1,style2.class1));
